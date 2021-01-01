69842f8907d38ea3e7387e90d274f030f1632f8c
iamngoni <imngonii@gmail.com>
2021-01-01 18:22:08 +0200

    validation

--- src/services/bulksms.ts
+++ src/services/bulksms.ts
@@ -1,5 +1,6 @@
 import Api from './api';
 import ApiResponse from '../types/api_response';
+import ApiError from '../types/api_error';
 
 export default class BulkSmsZw {
   private BULKSMS_WEBSERVICE_URL: string = 'http://portal.bulksmsweb.com/index.php?app=ws';
@@ -24,7 +25,47 @@ export default class BulkSmsZw {
    */
   async send(input: messageInput): Promise<ApiResponse> {
 
+    if (input.message === undefined) {
+      const error = new Error('Message is required');
+      return new ApiResponse({
+        api_response: new ApiError({
+          error: error.toString(),
+          error_string: 'Message missing',
+          status: 'ERR',
+          timestamp: Number(new Date().getTime())
+        }),
+        message: 'Message missing',
+        statusresponse: 'missing_input_error'
+      });
+    }
 
+    if (input.recipients === undefined) {
+      const error = new Error('Recipients are required');
+      return new ApiResponse({
+        api_response: new ApiError({
+          error: error.toString(),
+          error_string: 'Recipients missing',
+          status: 'ERR',
+          timestamp: Number(new Date().getTime())
+        }),
+        message: 'Recipients missing',
+        statusresponse: 'missing_input_error'
+      });
+    }
+
+    if (input.recipients.length < 1) {
+      const error = new Error('Recipients list is too short');
+      return new ApiResponse({
+        api_response: new ApiError({
+          error: error.toString(),
+          error_string: 'Recipients missing',
+          status: 'ERR',
+          timestamp: Number(new Date().getTime())
+        }),
+        message: 'Recipients missing',
+        statusresponse: 'missing_input_error'
+      });
+    }
 
     let url = this.sms_op();
 
